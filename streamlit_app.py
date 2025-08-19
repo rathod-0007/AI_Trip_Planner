21eb77628667f71b52d2f7785ecc9d2df08ed83e
rathod-0007 <rathodpavan2292@gmail.com>
2025-08-20 01:22:01 +0530

    tool flow updated

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import sys
 BASE_URL = "http://localhost:8000"  # Backend endpoint
 
 st.set_page_config(
-    page_title="🌍 Travel Planner Agentic Application",
+    page_title="Travel Planner Agentic Application",
     page_icon="🌍",
     layout="centered",
     initial_sidebar_state="expanded",
